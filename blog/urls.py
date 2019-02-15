5b7913a092adaead8db3dc4d47b406a7a82518d6
Edward <edwardlshfzw@gmail.com>
2019-02-15 09:51:38 +0800

    路由分发

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, re_path
+
+from blog import views
+from blog.views import slide_code_auth
+
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+urlpatterns = [
+    # 首页
+    re_path('index/', views.index, name='index'),
+
+    # 登陆页面和滑动验证码
+    path('login/', views.login),
+    re_path(r'^pc-geetest/register', slide_code_auth, name='pcgetcaptcha'),
+
+    # 注册页面
+    path('register/', views.register, name='register')
+
+]
--- cnblog/urls.py
+++ cnblog/urls.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
-from django.urls import path, re_path
-
-from blog import views
-from blog.views import slide_code_auth
+from django.urls import path, re_path, include
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # 首页
-    re_path('index/', views.index, name='index'),
-
-    # 滑动验证码
-    path('login/', views.login),
-    re_path(r'^pc-geetest/register', slide_code_auth, name='pcgetcaptcha'),
+    re_path(r'^', include(('blog.urls', 'blog')))
 
 ]
